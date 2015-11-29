e2ab10c9f528d099bbf765721a13a374278504a7
Jose Manuel Ramirez Martinez <ramirezmj1989@gmail.com>
2015-11-29 18:40:45 +0100

    Initial Commit

---
+++ BaseProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BaseProject
+//
+//  Created by Jose Manuel Ramírez Martínez on 29/11/15.
+//  Copyright © 2015 Jose Manuel Ramírez Martínez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BaseProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BaseProject
+//
+//  Created by Jose Manuel Ramírez Martínez on 29/11/15.
+//  Copyright © 2015 Jose Manuel Ramírez Martínez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
